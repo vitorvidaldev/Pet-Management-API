e2a372c235c84f4ae99ee7d467db98e57c5881cf
Vitor <vitorvidal.dev@gmail.com>
2021-02-17 18:56:15 -0300

    Started implementing tests for the user module

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { User } from "./user.entity";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
@@ -6,15 +8,47 @@ describe("Users Controller", () => {
   let usersController: UsersController;
   let usersService: UsersService;
 
+  const testNewUser: CreateUserDto = {
+    email: "teste@gmail.com",
+    password: "Teste123...",
+  };
+
+  const testUser: Partial<User> = {
+    email: "teste@gmail.com",
+    password: "Teste123...",
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            createUser: jest.fn().mockReturnValue([testUser]),
+          },
+        },
+      ],
     }).compile();
 
     usersService = module.get<UsersService>(UsersService);
     usersController = module.get<UsersController>(UsersController);
   });
 
-  describe("create new account", () => {});
+  it("Controller should be defined", () => {
+    expect(usersController).toBeDefined();
+  });
+
+  it("Service should be defined", () => {
+    expect(usersService).toBeDefined();
+  });
+
+  describe("Create new account", () => {
+    it("Should create a new user", async () => {
+      await expect(usersController.createUser(testNewUser)).resolves.toEqual({
+        id: "a uuid",
+        ...testNewUser,
+      });
+    });
+  });
 });

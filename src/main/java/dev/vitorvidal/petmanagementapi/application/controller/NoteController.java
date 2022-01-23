f42978615eab4135b8b05f234559cfd915f2214c
Vitor Vidal <vitorvidal.dev@gmail.com>
2022-01-23 17:56:08 -0300

    Initial config

---
+++ src/main/java/dev/vitorvidal/petmanagementapi/application/controller/NoteController.java
@@ -0,0 +1,9 @@
+package dev.vitorvidal.petmanagementapi.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/v1/note")
+public class NoteController {
+}
---
+++ src/main/java/dev/vitorvidal/petmanagementapi/application/service/NoteService.java
@@ -0,0 +1,7 @@
+package dev.vitorvidal.petmanagementapi.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+}

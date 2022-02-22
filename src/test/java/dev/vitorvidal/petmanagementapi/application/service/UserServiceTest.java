8bfa1ab5c107743923ff999efa1443aa981d68c7
Vitor Vidal <vitorvidal.dev@gmail.com>
2022-02-21 21:00:13 -0300

    Adding unit tests

--- src/test/java/dev/vitorvidal/petmanagementapi/application/service/UserServiceTest.java
+++ src/test/java/dev/vitorvidal/petmanagementapi/application/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package dev.vitorvidal.petmanagementapi.application.service;
 
+import dev.vitorvidal.petmanagementapi.model.dto.CreateUserDTO;
 import dev.vitorvidal.petmanagementapi.model.dto.UserDTO;
 import dev.vitorvidal.petmanagementapi.model.entity.UserEntity;
 import dev.vitorvidal.petmanagementapi.model.repository.UserRepository;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -54,6 +56,36 @@ class UserServiceTest {
 
     @Test
     void shouldSignupCorrectly() {
+        CreateUserDTO createUserDTOMock = mock(CreateUserDTO.class);
+        UserEntity userEntityMock = mock(UserEntity.class);
+
+        UUID userIdMock = UUID.randomUUID();
+        String usernameMock = "Test username";
+        String emailMock = "test@gmail.com";
+        String passwordMock = "secretPassword";
+        boolean isActiveMock = true;
+        LocalDateTime creationDateMock = LocalDateTime.now();
+
+        when(createUserDTOMock.username()).thenReturn(usernameMock);
+        when(createUserDTOMock.email()).thenReturn(emailMock);
+        when(createUserDTOMock.password()).thenReturn(passwordMock);
+
+        when(userEntityMock.getUserId()).thenReturn(userIdMock);
+        when(userEntityMock.getEmail()).thenReturn(emailMock);
+        when(userEntityMock.getIsActive()).thenReturn(isActiveMock);
+        when(userEntityMock.getCreationDate()).thenReturn(creationDateMock);
+
+        when(userRepository.save(any(UserEntity.class))).thenReturn(userEntityMock);
+
+        UserDTO signup = userService.signup(createUserDTOMock);
+
+        verify(userRepository, times(1)).save(any(UserEntity.class));
+
+        assertNotNull(signup);
+        assertEquals(userIdMock, signup.id());
+        assertEquals(emailMock, signup.email());
+        assertEquals(isActiveMock, signup.isActive());
+        assertEquals(creationDateMock, signup.creationDate());
     }
 
     @Test

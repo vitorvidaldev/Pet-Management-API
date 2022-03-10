bf0c8f2acd445b6762a471c9551f342181c5821d
Vitor Vidal <vitorvidal.dev@gmail.com>
2022-03-09 21:47:45 -0300

    Adding a page size in repository, when listing pets and notes

--- src/main/java/dev/vitorvidal/petmanagementapi/model/repository/PetRepository.java
+++ src/main/java/dev/vitorvidal/petmanagementapi/model/repository/PetRepository.java
@@ -2,13 +2,13 @@ package dev.vitorvidal.petmanagementapi.model.repository;
 
 import dev.vitorvidal.petmanagementapi.model.entity.PetEntity;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PetRepository extends CassandraRepository<PetEntity, UUID> {
-    Optional<List<PetEntity>> findByUserId(UUID userId);
+    Slice<PetEntity> findByUserId(UUID userId, Pageable pageable);
 }
